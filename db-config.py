eb24c3772825728e12ea2b4879819fa662ef77ef
Souvede Inshuti <isouvede2@gmail.com>
2024-04-13 00:59:30 -0700

    Added working database configuration

--- db-config.py
+++ db-config.py
@@ -1,27 +1,34 @@
 #remember to install psycopg2
+import psycopg2
 conn = psycopg2.connect(
     host="localhost",
     database="ticktack",
-    user="postgress",
-    password="your_password"
+    user="postgres",
+    password="postgres"
 )
 
 #open cursor to perform db operations
 cur = conn.cursor()
-cur.execute(""" CREATE TABLE events(
+cur.execute(""" CREATE TABLE IF NOT EXISTS events(
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     description VARCHAR(50),
-    from TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    to TIMESTAMP WITH TIME ZONE,
+    from_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    to_time TIMESTAMP WITH TIME ZONE,
     priority INTEGER DEFAULT 0
+);
+""")
+cur.execute(""" CREATE TABLE IF NOT EXISTS timers(
+    id SERIAL PRIMARY KEY,
+    from_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    to_time TIMESTAMP WITH TIME ZONE
 );
 """)
 
 # some first record into our table that can be used to test
-cur.execute('INSERT INTO events(name, description, from, to) VALUES("presentation","I will have a one-to-one presentation",TO_TIMESTAMP('2024 04 11 11:45','YY MM DD HH:MI'),TO_TIMESTAMP('2024 04 11 12:45','YY MM DD HH:MI'))');
-cur.execute('INSERT INTO events(name, from, to) VALUES("class",TO_TIMESTAMP('2024 05 11 11:45','YY MM DD HH:MI'),TO_TIMESTAMP('2024 05 11 12:45','YY MM DD HH:MI'))');
-cur.execute('INSERT INTO events(name, description, from, to) VALUES("meeting","I will have a meeting with my team",TO_TIMESTAMP('2024 04 11 10:00','YY MM DD HH:MI'),TO_TIMESTAMP('2024 04 11 11:30','YY MM DD HH:MI'))');
+# cur.execute("INSERT INTO events(name, description,from_time, to_time) VALUES('presentation','I will have a one-to-one presentation',TO_TIMESTAMP('2024 04 11 11:45','YY MM DD HH:MI'),TO_TIMESTAMP('2024 04 11 12:45','YY MM DD HH:MI'))")
+# cur.execute("INSERT INTO events(name, description, from_time, to_time) VALUES('meeting','I will have a meeting with my team',TO_TIMESTAMP('2024 04 11 10:00','YY MM DD HH:MI'),TO_TIMESTAMP('2024 04 11 11:30','YY MM DD HH:MI'))")
+
 
 #make changes to the database persistent
 conn.commit()
